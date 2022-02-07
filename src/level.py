87a19798686862196e517c5239e2b15b1fd16b74
rodrigo mardones <rignmardones@falabella.cl>
2022-02-07 10:37:57 -0300

    ysort camera

--- src/level.py
+++ src/level.py
@@ -7,7 +7,7 @@ from debug import debug
 class Level:
     def __init__ (self):
         self.display_surface = pygame.display.get_surface()
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
         self.create_map()
 
@@ -19,10 +19,23 @@ class Level:
                 if(col == 'x'):
                     Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
                 if(col == 'p'):
-                    self.player = Player((x,y), [self.visible_sprites])
+                    self.player = Player((x,y), [self.visible_sprites],self.obstacles_sprites)
 
 
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw()
         self.visible_sprites.update()
         debug(self.player.direction)
+
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2(0,0)
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            offset_rect = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image,offset_rect)
--- src/player.py
+++ src/player.py
@@ -1,18 +1,39 @@
 import pygame
 from debug import debug
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacles):
         super().__init__(groups)
         self.image = pygame.image.load('src/assets/testAssets/player.png');
         self.rect = self.image.get_rect(topleft = pos)
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.obstacles = obstacles
 
     def move(self, speed):
         # normalizado de velocidad diagonal
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-        self.rect.center += self.direction * speed
+
+        self.rect.x += self.direction.x * speed
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * speed
+        self.collision("vertical")
+
+    def collision(self, direction):
+        if(direction == "horizontal"):
+            for obstacle in self.obstacles:
+                if obstacle.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = self.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = self.rect.right
+        if(direction == 'vertical'):
+            for obstacles in self.obstacles:
+                if obstacles.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = self.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = self.rect.bottom
 
     def input(self):
         # inputs del jugador
